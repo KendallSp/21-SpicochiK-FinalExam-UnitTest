406a849c531ec26d613aef243117d5cf68ab4dff
Kendall Spicochi <kendallspicochi@gmail.com>
2022-06-21 12:50:15 -0700

    added functionality for add method. All tests pass

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,10 +1,16 @@
 public class StringCalculator {
-    public static int add(final String numbers) {
+    public static int add(final String numbers) throws IllegalArgumentException{
         int returnValue = 0;
         String[] numbersArray = numbers.split(",|\n");
         for (String number : numbersArray) {
             if (!number.trim().isEmpty()) {
-                returnValue += Integer.parseInt(number.trim());
+                //checks if number is less than 1000, else ignore
+                if(Integer.parseInt(number.trim())<0){
+                    throw new IllegalArgumentException("negatives not allowed");
+                }
+                if(Integer.parseInt(number.trim())<1000) {
+                    returnValue += Integer.parseInt(number.trim());
+                }
             }
         }
         return returnValue;
